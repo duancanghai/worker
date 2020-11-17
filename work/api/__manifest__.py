47d32560318045c7fff0cd163f7e54ad819b06e3
段 <74281791+duancanghai@users.noreply.github.com>
2020-11-17 18:37:12 +0800

    Update __manifest__.py

--- work/api/__manifest__.py
+++ work/api/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Enterprise WeChat API",
-    "author": "RStudio",
+    "author": "Rs",
     "website": "",
     "sequence": 601,
     "installable": True,
     "application": True,
     "auto_install": False,
-    "category": "Enterprise WeChat/Enterprise WeChat",
+    "category": "Enterprise WeChat",
     "version": "14.0.0.1",
     "summary": """
         Enterprise WeChat API
